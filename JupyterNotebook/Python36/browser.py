e4517dca2f5b7c19753bcd3928bb2b022a94a2a4
tongxiao <tongxiao@yeah.net>
2018-05-14 13:57:27 +0800

    使用上下文管理器替换单例模式，方便自动释放资源

--- JupyterNotebook/Python36/browser.py
+++ JupyterNotebook/Python36/browser.py
@@ -6,48 +6,28 @@
 
 浏览器对象
 """
-import threading
-
 from selenium import webdriver
 
 
 class Browser(object):
     """
-    浏览器对象，基于selenium，单例模式，多线程安全，自动回收驱动对象。
+    浏览器对象，基于selenium，单例模式，自动回收驱动对象。
 
     最新的selenium声称不支持PhantomJS了，考虑到服务器上运行的问题，这里使用2.53.6版本的selenium。
     使用之前，除了安装selenium（pip install selenium==2.53.6）之外，还需要安装PhantomJS，并配置到系统环境变量中。
     PhantomJS下载地址：http://phantomjs.org/download.html
 
     使用示例:
-    b = Browser()
-    b.browser.get('https://www.baidu.com/')
+    with Browser() as browser:
+        browser.get('https://www.baidu.com/')
     """
 
-    _mutex = threading.Lock()
+    def __enter__(self):
+        self.browser = webdriver.PhantomJS(service_log_path='./log_file/browser.log')
+        self.browser.viewportSize = {'width': 1366, 'height': 768}
+        self.browser.maximize_window()
+        return self.browser
 
-    def __new__(cls, *args, **kwargs):
-        """
-        实例化方法
-        :param args: 参数
-        :param kwargs: 关键字
-        :return: 实例
-        """
-        if not hasattr(Browser, '_instance'):
-            with Browser._mutex:
-                if not hasattr(Browser, '_instance'):
-                    Browser._instance = object.__new__(cls)
-                    Browser._instance.browser = webdriver.PhantomJS()
-                    Browser._instance.browser.viewportSize = {'width': 1366, 'height': 768}
-                    Browser._instance.browser.maximize_window()
-        return Browser._instance
-
-    def __del__(self):
-        """
-        析构方法
-        :return: 无
-        """
-        if hasattr(self, 'browser'):
-            with Browser._mutex:
-                if hasattr(self, 'browser'):
-                    self.browser.quit()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.browser:
+            self.browser.quit()
